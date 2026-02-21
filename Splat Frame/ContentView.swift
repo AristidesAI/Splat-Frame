2e185ef58fefaeb14094384b43461c30f8b8f028
AristidesAI <aristideslintzeris@icloud.com>
2026-02-21 11:32:04 +1100

    Initial Commit

---
+++ Splat Frame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Splat Frame
+//
+//  Created by aristides lintzeris on 21/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Splat Frame/Splat_FrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Splat_FrameApp.swift
+//  Splat Frame
+//
+//  Created by aristides lintzeris on 21/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Splat_FrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
